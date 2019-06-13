5362905837770d256196d5fb1cd4455bcb7a1ff7
Marcin Dziadon <marcin.dziadon@coderslab.pl>
2019-06-13 15:00:52 +0200

    Servlety zadania mvc

---
+++ src/main/java/pl/coderslab/mvc/Mvc14.java
@@ -0,0 +1,37 @@
+package pl.coderslab.mvc;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet("/Mvc14")
+public class Mvc14 extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Book> books = new ArrayList<>();
+
+        String[] titles = request.getParameterValues("title");
+        String[] authors = request.getParameterValues("author");
+        String[] isbns = request.getParameterValues("isbn");
+
+        for (int i = 0; i < titles.length ; i++) {
+            Book book = new Book(titles[i], authors[i], isbns[i]);
+            books.add(book);
+        }
+
+        request.setAttribute("books", books);
+
+        getServletContext().getRequestDispatcher("/result14.jsp")
+                .forward(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/mvc14.jsp")
+                .forward(request, response);
+    }
+}
